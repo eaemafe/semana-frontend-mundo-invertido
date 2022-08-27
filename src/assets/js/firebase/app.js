4251ab500c6267774494ec71245db2921c78147e
eaemafe <maria.kbst_17@hotmail.com>
2022-08-27 16:32:12 -0300

    Integração com o firebase

---
+++ src/assets/js/firebase/app.js
@@ -0,0 +1,19 @@
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.3/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyCz3tWWNA9IBixNjXLanIEKx0CxNir3MK0",
+    authDomain: "semana-mundo-invertido-4ef52.firebaseapp.com",
+    projectId: "semana-mundo-invertido-4ef52",
+    storageBucket: "semana-mundo-invertido-4ef52.appspot.com",
+    messagingSenderId: "742896329335",
+    appId: "1:742896329335:web:12427aaecaf35afe935c72"
+  }
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig)
+  export default app
